e6bfb9419767c207f3684bd2f9ca9df7fb776b02
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-05-01 00:31:39 +0200

    Liquibase (#8)
    * table create
    
    * workflows
    
    * mariadb
    
    * fix
    
    * foreign keys

--- mariadb/changesets/1-tables.sql
+++ mariadb/changesets/1-tables.sql
@@ -1,17 +1,17 @@
 --liquibase formatted sql
 
 -- changeset liquibase:create_user_table
-CREATE TABLE zset_uwachowski.user (
+CREATE TABLE user (
   `id` VARCHAR(256) UNIQUE PRIMARY KEY NOT NULL,
   `role` ENUM('admin','user') NOT NULL DEFAULT 'user',
   `email` TEXT NOT NULL,
   `enabled` BOOLEAN NOT NULL DEFAULT 0,
   `timestamp` BIGINT(20) NOT NULL DEFAULT ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000,0)
 );
--- rollback DROP TABLE zset_uwachowski.user;
+-- rollback DROP TABLE user;
 
 -- changeset liquibase:create_project_table
-CREATE TABLE zset_uwachowski.project (
+CREATE TABLE project (
   `id` VARCHAR(36) UNIQUE PRIMARY KEY NOT NULL,
   `user_id` VARCHAR(256) NOT NULL,
   `status_id` VARCHAR(36) NOT NULL,
@@ -19,36 +19,36 @@ CREATE TABLE zset_uwachowski.project (
   `content` LONGTEXT NOT NULL,
   `timestamp` BIGINT(20) NOT NULL DEFAULT ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000,0)
 );
--- rollback DROP TABLE zset_uwachowski.project;
+-- rollback DROP TABLE project;
 
 -- changeset liquibase:create_connections_table
-CREATE TABLE zset_uwachowski.connections (
+CREATE TABLE connections (
   `connection_id` VARCHAR(256) UNIQUE PRIMARY KEY NOT NULL,
   `user_id` VARCHAR(256) NOT NULL
 );
--- rollback DROP TABLE zset_uwachowski.connections;
+-- rollback DROP TABLE connections;
 
 -- changeset liquibase:create_category_table
-CREATE TABLE zset_uwachowski.category (
+CREATE TABLE category (
   `id` VARCHAR(36) UNIQUE PRIMARY KEY NOT NULL,
   `name` TINYTEXT NOT NULL,
   `color` MEDIUMINT(8) UNSIGNED NOT NULL,
   `visible` BOOLEAN NOT NULL
 );
--- rollback DROP TABLE zset_uwachowski.category;
+-- rollback DROP TABLE category;
 
 -- changeset liquibase:create_project_category_table
-CREATE TABLE zset_uwachowski.project_category (
+CREATE TABLE project_category (
  `project_id` VARCHAR(36) NOT NULL,
  `category_id` VARCHAR(36) NOT NULL
 );
--- rollback DROP TABLE zset_uwachowski.project_category;
+-- rollback DROP TABLE project_category;
 
 -- changeset liquibase:status_table
-CREATE TABLE zset_uwachowski.status (
+CREATE TABLE status (
   `id` VARCHAR(36) UNIQUE PRIMARY KEY NOT NULL,
   `name` TINYTEXT NOT NULL,
   `color` MEDIUMINT(8) UNSIGNED NOT NULL,
   `visible` BOOLEAN NOT NULL
 );
--- rollback DROP TABLE zset_uwachowski.status;
+-- rollback DROP TABLE status;
