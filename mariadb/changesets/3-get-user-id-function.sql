80919b382f3e3431f228fda37f896b8fb4bbb3fe
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-05-02 00:17:48 +0200

    Liquibase (#11)
    * table create
    
    * workflows
    
    * mariadb
    
    * fix
    
    * getUserId fn
    
    * categories procedures
    
    * fix rebase

--- mariadb/changesets/3-get-user-id-function.sql
+++ mariadb/changesets/3-get-user-id-function.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 -- changeset liquibase:get_user_id endDelimiter://
-CREATE FUNCTION `getUserId`(`in_id` VARCHAR(256)) 
+CREATE FUNCTION `getUserId`(`in_id` VARCHAR(256)) RETURNS VARCHAR(256)
 BEGIN 
 DECLARE out_user_id VARCHAR(256); 
 SELECT user_id INTO out_user_id FROM connections WHERE connection_id = in_id LIMIT 1; 
