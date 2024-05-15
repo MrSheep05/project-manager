a6e599abaad0e579714af79b7cc79f2a14fc2003
Filip Wachowski <fifiw2005@gmail.com>
2024-05-15 20:56:43 +0200

    procedure

--- web/src/views/projects/index.tsx
+++ web/src/views/projects/index.tsx
@@ -5,6 +5,7 @@ import { SendAction } from "../../hooks/websocket/types";
 const Projects = () => {
   const { isAvailable, send, state } = useContext(WebsocketState);
   useEffect(() => console.log(state), [state]);
+
   return <div>ISAvailable {isAvailable ? "TAK" : "NIE"}</div>;
 };
 
