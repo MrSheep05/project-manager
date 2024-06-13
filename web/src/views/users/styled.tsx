ae4f17c85a69667b404995f055036d103777f7d8
KoVaL <lukikowal05@gmail.com>
2024-06-13 17:12:59 +0200

    User List

---
+++ web/src/views/users/styled.tsx
@@ -0,0 +1,9 @@
+import { Box, styled } from "@mui/material";
+
+export const StyledBox = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "left",
+  alignItems: "left",
+  width: "100%",
+}));
