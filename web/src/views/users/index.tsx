c18f14e6ef116f31e422f3d8ba72d00b2ecf6c54
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-06-13 19:31:21 +0200

    User page (#53)
    * User List
    
    * AddProject outlet
    
    * status view
    
    * admin-panel
    
    * without outlet
    
    * allUseres procedure
    
    ---------
    
    Co-authored-by: KoVaL <lukikowal05@gmail.com>

--- web/src/views/users/index.tsx
+++ web/src/views/users/index.tsx
@@ -2,17 +2,67 @@ import { useContext, useEffect } from "react";
 import useAdminRoute from "../../hooks/useAdminRoute";
 import { WebsocketState } from "../../hooks/websocket";
 import { SendAction } from "../../hooks/websocket/types";
+import { Box, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import AutoSizer, { Size } from "react-virtualized-auto-sizer";
+import { UserBody } from "../../utils/types";
+import { StyledBox } from "./styled";
+
+function renderRow(index: number, users: UserBody[]) {
+  const user = users[index];
+
+  const ListStyle = {
+    color: "black",
+  };
+
+  return (
+    <ListItem style={ListStyle} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`${user.email}`} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
 
 const Users = () => {
   const { state, send } = useContext(WebsocketState);
   useAdminRoute();
 
   useEffect(() => {
-    if (state.users.length === 0)
+    if (state.users.length === 0) {
       send({ action: SendAction.GetUsers, payload: {} });
-  }, [state]);
+    }
+  }, [state, send]);
 
-  return <p>USERS</p>;
+  return (
+    <StyledBox>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "100vh",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          overflow: "hidden",
+          border: "1px solid rgba(0, 0, 0, 0.05)",
+        }}
+      >
+        <AutoSizer>
+          {({ height, width }: Size) => (
+            <FixedSizeList
+              height={height}
+              width={width}
+              itemCount={state.users.length}
+              itemSize={35}
+            >
+              {(props) => renderRow(props.index, state.users)}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
+      </Box>
+    </StyledBox>
+  );
 };
 
 export default Users;
